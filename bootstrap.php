d3047e25ec7415febfc1b2384c1ba5ef0c6a9a43
Jorge Orpinel <jorge@orpinel.com>
2017-02-02 00:41:18 -0500

    PHPUnit Bootstrapping and Autoload via phpunit.xml <testsuite>

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+// app/tests.bootstrap.php
+if (isset($_ENV['BOOTSTRAP_CLEAR_CACHE_ENV'])) {
+    passthru(sprintf(
+        'php "%s/console" cache:clear --env=%s --no-warmup',
+        __DIR__,
+        $_ENV['BOOTSTRAP_CLEAR_CACHE_ENV']
+    ));
+}
+
+require __DIR__.'/src/Controllers/BaseController.php';
+?>
