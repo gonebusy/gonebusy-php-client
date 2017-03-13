42002067c43db3185f2a151208e6fbc7c0753863
Jorge Orpinel <jorge@orpinel.com>
2017-03-13 01:55:30 -0400

    Adding ServicesTest with test for createService()

---
+++ test/Controllers/ServicesTest.php
@@ -0,0 +1,155 @@
+<?php
+/*
+ * Resources SDK Controller Test Case
+ */
+
+namespace GonebusyTest\Controllers;
+
+use PHPUnit\Framework\TestCase;
+
+use GonebusyLib\Configuration;
+use GonebusyLib\GonebusyClient;
+use GonebusyLib\Models\CreateServiceBody;
+use GonebusyLib\Models\UpdateServiceByIdBody;
+
+class ServicesTest extends TestCase
+{
+    /**
+     * To contain the GonebusyLib\GonebusyClient
+     */
+    protected $client;
+
+    /**
+     * To contain the GonebusyLib\Controllers\ServicesController
+     */
+    protected $services;
+
+    /**
+     * Create the GoneBusy SDK client for each test.
+     */
+    public function setUp() {
+        $this->client = new GonebusyClient();
+        $this->services = $this->client->getServices();
+    }
+
+
+    /**
+     * Returns arbitrary service data.
+     * @param  string $type Should be 'create' or 'update'.
+     * @return  CreateServiceBody or UpdateServiceByIdBody object with unique data to send to API
+     */
+    private function serviceBody($type) {
+        $rand = rand();
+        switch($type) {
+            case 'create':
+                return new CreateServiceBody(
+                    "description", // REQUIRED
+                    15, // duration REQUIRED XXX 0 not accepted but NULL turns into 0
+                    "name", // REQUIRED
+                    NULL, // categories
+                    NULL, // price_model_id XXX defaults to 28
+                    NULL, // resources defaults to self Resource
+                    "short_name",
+                    NULL // user_id defaults to self User
+                );
+            case 'update':
+                return new UpdateServiceByIdBody(
+                    "categories",
+                    "description",
+                    NULL, // duration
+                    "name",
+                    NULL, // price_model_id
+                    "resources",
+                    "short_name"
+                );
+        }
+    }
+
+    /**
+     * Generate data from Service response.
+     * @param  GonebusyLib\Models\EntitiesServiceResponse $response
+     * @param  string $type Should be 'create' or 'update'.
+     * @return  CreateServiceBody or UpdateServiceByIdBody object with data from $response
+     */
+    private function bodyFromResponse($response, $type) {
+        switch($type) {
+            case 'create':
+                return new CreateServiceBody(
+                    $response->service->description,
+                    $response->service->duration,
+                    $response->service->name,
+                    NULL, // $response->service->categories,
+                    NULL, // $response->service->priceModelId,
+                    NULL, // $response->service->resources,
+                    $response->service->shortName,
+                    $response->service->userId
+                );
+            case 'update':
+                return new UpdateServiceByIdBody(
+                    $response->service->capacity,
+                    $response->service->description,
+                    $response->service->gender,
+                    $response->service->name,
+                    $response->service->thingTypeId
+                );
+        }
+    }
+
+
+    /**
+     * Test POST /services/new
+     * GonebusyLib\Controllers\ServicesController::createService()
+     */
+    public function testCreateService() {
+        $createServiceBody = $this->serviceBody('create');
+
+        // Create GonebusyLib\Models\EntitiesServiceResponse:
+        $response = $this->services->createService(Configuration::$authorization, $createServiceBody);
+
+        // Was it created?
+        $this->assertInstanceOf('GonebusyLib\Models\CreateServiceResponse', $response);
+
+        // Does it have all the original data we sent?
+        $responseBody = $this->bodyFromResponse($response, 'create');
+        $this->assertEquals($responseBody, $createServiceBody);
+
+        // Delete test service:
+        $delResponse = $this->services->deleteServiceById(Configuration::$authorization, $response->service->id);
+    }
+
+    /**
+     * @todo
+     */
+    public function testUpdateServiceById() {
+        //
+    }
+
+    /**
+     * @todo
+     */
+    public function testGetServiceById() {
+        //
+    }
+
+    /**
+     * @todo
+     */
+    public function testGetServiceAvailableSlotsById() {
+        //
+    }
+
+    /**
+     * @todo
+     */
+    public function testGetServices() {
+        //
+    }
+
+    /**
+     * @todo
+     */
+    public function testDeleteServiceById() {
+        //
+    }
+
+}
