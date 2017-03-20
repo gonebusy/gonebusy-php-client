7f3e6196ecb203ebe7f1d28e50b5144ed45252d2
Jorge Orpinel <jorge@orpinel.com>
2017-03-20 01:10:04 -0400

    Time Windows -- testUpdateScheduleTimeWindowById is broken!

--- test/Controllers/SchedulesTest.php
+++ test/Controllers/SchedulesTest.php
@@ -44,6 +44,7 @@ class SchedulesTest extends TestCase
         $this->resources = $this->client->getResources();
     }
 
+
     /**
      * Generate arbitrary service or resource data.
      * @param  string $action 'Service' or 'Resource'
@@ -108,28 +109,28 @@ class SchedulesTest extends TestCase
         switch($action) {
             case 'create':
                 return new CreateScheduleTimeWindowBody(
-                    "sunday", // days
-                    "09:30", // end_time
-                    "once", // recurs_by
-                    "2017-03-12", // start_date
-                    "09:00", // start_time
+                    "sunday, monday, tuesday, wednesday, thursday, friday, saturday", // days
+                    "18:00", // end_time
+                    "daily", // recurs_by
+                    date('Y-m-d', strtotime('tomorrow')), // start_date
+                    "12:00", // start_time
                     NULL, // date_recurs_by
-                    NULL, // end_date
-                    "single", // frequency
+                    date('Y-m-d', strtotime('tomorrow')), // end_date
+                    NULL, // frequency
                     NULL, // occurrence
                     NULL // total_minutes
                 );
             case 'update':
                 return new UpdateScheduleTimeWindowByIdBody(
                     NULL, // date_recurs_by
-                    "sunday", // days
-                    NULL, // end_date
-                    "09:30", // end_time
-                    "single", // frequency
+                    "monday, tuesday, wednesday, thursday, friday", // other days
+                    date('Y-m-d', strtotime('tomorrow + 1 day')), // another end_date
+                    "19:00", // anther end_time
+                    NULL, // frequency
                     NULL, // occurrence
-                    "once", // recurs_by
-                    "2017-03-12", // start_date
-                    "09:00", // start_time
+                    "monthly", // another recurs_by
+                    date('Y-m-d', strtotime('tomorrow + 1 day')), // anther start_date
+                    "13:00", // anther start_time
                     NULL // total_minutes
                 );
         }
@@ -138,16 +139,16 @@ class SchedulesTest extends TestCase
     /**
      * Generate data from Schedule response.
      * @param  GonebusyLib\Models\EntitiesScheduleResponse $response
-     * @param  string $type 'Schedule', 'create', or 'update' (ScheduleTimeWindow)
+     * @param  string $type 'CreateSchedule', 'createSTW', or 'updateSTW' (ScheduleTimeWindow)
      * @return  mixed object with data from $response
      */
     private function bodyFromResponse($response, $type) {
         switch($type) {
-            case 'Schedule':
+            case 'CreateSchedule':
                 return new CreateScheduleBody(
                     $response->schedule->serviceId,
                     NULL, // $response->schedule->dateRecursBy,
-                    "sunday, monday, tuesday, wednesday, thursday, friday, saturday", // days,
+                    "sunday, monday, tuesday, wednesday, thursday, friday, saturday", // XXX days,
                     $response->schedule->timeWindows[0]->endDate,
                     $response->schedule->timeWindows[0]->endTime,
                     NULL, // $response->schedule->timeWindows[0]->frequency,
@@ -159,62 +160,65 @@ class SchedulesTest extends TestCase
                     NULL, // $response->schedule->timeWindows[0]->totalMinutes,
                     NULL // $response->schedule->ownerId
                 );
-            case 'create':
+            case 'createSTW':
                 return new CreateScheduleTimeWindowBody(
-                    $response->schedule->days,
-                    $response->schedule->endTime,
-                    $response->schedule->recursBy,
-                    $response->schedule->startDate,
-                    $response->schedule->startTime,
-                    $response->schedule->dateRecursBy,
-                    $response->schedule->endDate,
-                    $response->schedule->frequency,
-                    $response->schedule->occurrence,
-                    $response->schedule->totalMinutes
+                    "sunday, monday, tuesday, wednesday, thursday, friday, saturday", // XXX days,
+                    $response->schedule->timeWindows[1]->endTime,
+                    $response->schedule->timeWindows[1]->recursBy,
+                    $response->schedule->timeWindows[1]->startDate,
+                    $response->schedule->timeWindows[1]->startTime,
+                    $response->schedule->timeWindows[1]->dateRecursBy,
+                    $response->schedule->timeWindows[1]->endDate,
+                    NULL, // $response->schedule->timeWindows[1]->frequency,
+                    NULL, // $response->schedule->timeWindows[1]->occurrence,
+                    NULL // $response->schedule->timeWindows[1]->totalMinutes
                 );
-            case 'update':
+            case 'updateSTW':
                 return new UpdateScheduleTimeWindowByIdBody(
-                    $response->schedule->dateRecursBy,
-                    $response->schedule->days,
-                    $response->schedule->endDate,
-                    $response->schedule->endTime,
-                    $response->schedule->frequency,
-                    $response->schedule->occurrence,
-                    $response->schedule->recursBy,
-                    $response->schedule->startDate,
-                    $response->schedule->startTime,
-                    $response->schedule->totalMinutes
+                    $response->schedule->timeWindows[1]->dateRecursBy,
+                    "monday, tuesday, wednesday, thursday, friday", // XXX other days,
+                    $response->schedule->timeWindows[1]->endDate,
+                    $response->schedule->timeWindows[1]->endTime,
+                    NULL, // $response->schedule->timeWindows[1]->frequency,
+                    NULL, // $response->schedule->timeWindows[1]->occurrence,
+                    $response->schedule->timeWindows[1]->recursBy,
+                    $response->schedule->timeWindows[1]->startDate,
+                    $response->schedule->timeWindows[1]->startTime,
+                    NULL // $response->schedule->timeWindows[1]->totalMinutes
                 );
         }
     }
 
+
+    /*
+     * Schedules
+     */
 
     /**
      * Test POST /schedules/new
      * GonebusyLib\Controllers\SchedulesController::createSchedule()
      */
     public function testCreateSchedule() {
-        // Create Service
+        // Create Service:
         $createServiceBody = $this->createBody('Service');
         $serviceResponse = $this->services->createService(Configuration::$authorization, $createServiceBody);
-        // Create Resource
+        // Create Resource:
         $createResourceBody = $this->createBody('Resource');
         $resourceResponse = $this->resources->createResource(Configuration::$authorization, $createResourceBody);
 
-        // Create Schedule Body
-        $createSheduleBody = $this->scheduleBody(
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody(
             $serviceResponse->service->id,
             $resourceResponse->resource->id);
-
-        // Create GonebusyLib\Models\EntitiesScheduleResponse:
-        $response = $this->schedules->createSchedule(Configuration::$authorization, $createSheduleBody);
+        $response = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
 
         // Was it created?
         $this->assertInstanceOf('GonebusyLib\Models\CreateScheduleResponse', $response);
+        // $response->schedule is a GonebusyLib\Models\EntitiesScheduleResponse
 
-        // Does it have all the original data we sent?
-        $responseBody = $this->bodyFromResponse($response, 'Schedule');
-        $this->assertEquals($createSheduleBody, $responseBody);
+        // Does it have all the original data sent?
+        $responseBody = $this->bodyFromResponse($response, 'CreateSchedule');
+        $this->assertEquals($createScheduleBody, $responseBody);
 
         // Delete test schedule:
         $delResponse = $this->schedules->deleteScheduleById(Configuration::$authorization, $response->schedule->id);
@@ -225,45 +229,202 @@ class SchedulesTest extends TestCase
     }
 
     /**
+     * Test GET /schedules/{id}
+     * GonebusyLib\Controllers\SchedulesController::getScheduleById()
+     */
+    public function testGetScheduleById() {
+        $serviceResponse = $this->services->createService(Configuration::$authorization, $this->createBody('Service'));
+        $resourceResponse = $this->resources->createResource(Configuration::$authorization, $this->createBody('Resource'));
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
+        $responseSchedule = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
+
+        // Fetch same Schedule we just created:
+        $response = $this->schedules->getScheduleById(
+            Configuration::$authorization,
+            $responseSchedule->schedule->id
+        );
+
+        // Was it fetched?
+        $this->assertInstanceOf('GonebusyLib\Models\GetScheduleByIdResponse', $response);
+
+        // Does it have all the original data we sent?
+        $responseBody = $this->bodyFromResponse($response, 'CreateSchedule');
+        $this->assertEquals($responseBody, $createScheduleBody);
+
+        // Delete test entities:
+        $delResponse = $this->schedules->deleteScheduleById(Configuration::$authorization, $response->schedule->id);
+        $delResponseResource = $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
+        $delResponseService = $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
+    }
+
+    /**
      * Test GET /schedules
+     * Assumes there's at least 3 Schedules in the API's database.
      * GonebusyLib\Controllers\SchedulesController::getSchedules()
      */
     public function testGetSchedules() {
-        //
+        $perPage = 3;
+        $response = $this->schedules->getSchedules(
+            Configuration::$authorization,
+            $page = 1,
+            $perPage);
+
+        // Was it fetched?
+        $this->assertInstanceOf('GonebusyLib\Models\GetSchedulesResponse', $response);
+
+        // Did it return an array of 3 schedules?
+        $this->assertCount($perPage, $response->schedules);
+        foreach($response->schedules as $schedule) {
+            $this->assertInstanceOf('GonebusyLib\Models\GetSchedulesResponse', $response);
+        }
     }
 
     /**
-     * Test GonebusyLib\Controllers\SchedulesController::getScheduleById()
-     */
-    public function testGetScheduleById() {
-        //
-    }
-
-    /**
-     * @todo
-     */
-    public function testDeleteScheduleTimeWindowById() {
-        //
-    }
-
-    /**
-     * @todo
-     */
-    public function testCreateScheduleTimeWindow() {
-        //
-    }
-
-    /**
-     * @todo
+     * Test DELETE /schedules/{id}
+     * GonebusyLib\Controllers\SchedulesController::deleteScheduleById()
      */
     public function testDeleteScheduleById() {
-        //
+        $serviceResponse = $this->services->createService(Configuration::$authorization, $this->createBody('Service'));
+        $resourceResponse = $this->resources->createResource(Configuration::$authorization, $this->createBody('Resource'));
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
+        $responseSchedule = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
+
+        // Delete Schedule:
+        $response = $this->schedules->deleteScheduleById(Configuration::$authorization, $responseSchedule->schedule->id);
+        // XXX Returns a limited set compared to create and update
+
+        // Was it deleted?
+        $this->assertInstanceOf('GonebusyLib\Models\DeleteScheduleByIdResponse', $response);
+
+        $responseBody = $this->bodyFromResponse($response, 'CreateSchedule');
+        $this->assertEquals($responseBody->serviceId, $createScheduleBody->serviceId);
+        // The record previously created is the same as the one deleted.
+
+        // Delete remaining test entities:
+        $delResponseResource = $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
+        $delResponseService = $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
+    }
+
+
+    /*
+     * Time windows
+     */
+
+    /**
+     * Test POST /schedules/{id}/time_windows/new
+     * GonebusyLib\Controllers\SchedulesController::createScheduleTimeWindow()
+     */
+    public function testCreateScheduleTimeWindow() {
+        $serviceResponse = $this->services->createService(Configuration::$authorization, $this->createBody('Service'));
+        $resourceResponse = $this->resources->createResource(Configuration::$authorization, $this->createBody('Resource'));
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
+        $responseSchedule = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
+
+
+        // Create ScheduleTimeWindow:
+        $createSTWBody = $this->timeWindowBody('create');
+        $response = $this->schedules->createScheduleTimeWindow(
+            Configuration::$authorization,
+            $responseSchedule->schedule->id,
+            $createSTWBody
+        );
+
+        // Was it created?
+        $this->assertInstanceOf('GonebusyLib\Models\CreateScheduleTimeWindowResponse', $response);
+
+        // Does it have all the original data we sent?
+        $responseBody = $this->bodyFromResponse($response, 'createSTW');
+        $this->assertEquals($responseBody, $createSTWBody);
+
+
+        // Delete test entities:
+        $delResponse = $this->schedules->deleteScheduleTimeWindowById(Configuration::$authorization, $responseSchedule->schedule->id, $response->schedule->timeWindows[1]->id);
+        $delScheduleResponse = $this->schedules->deleteScheduleById(Configuration::$authorization, $responseSchedule->schedule->id);
+        $delResponseResource = $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
+        $delResponseService = $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
     }
 
     /**
-     * @todo
+     * Test PUT /schedules/{id}/time_windows/{time_window_id}
+     * GonebusyLib\Controllers\SchedulesController::updateScheduleTimeWindowById()
      */
     public function testUpdateScheduleTimeWindowById() {
+        $serviceResponse = $this->services->createService(Configuration::$authorization, $this->createBody('Service'));
+        $resourceResponse = $this->resources->createResource(Configuration::$authorization, $this->createBody('Resource'));
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
+        $responseSchedule = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
+
+        // Create ScheduleTimeWindow:
+        $createSTWBody = $this->timeWindowBody('create');
+        $responseSTW = $this->schedules->createScheduleTimeWindow(Configuration::$authorization, $responseSchedule->schedule->id, $createSTWBody);
+
+
+        // Update the same time window:
+        $anotherSTWBody = $this->timeWindowBody('update');
+        // print_r($anotherSTWBody);
+        $response = $this->schedules->updateScheduleTimeWindowById(
+            Configuration::$authorization,
+            $responseSchedule->schedule->id,
+            $responseSTW->schedule->timeWindows[1]->id,
+            $anotherSTWBody);
+        print_r($response);
+
+        // // Was it updated?
+        // $this->assertInstanceOf('GonebusyLib\Models\UpdateScheduleTimeWindowResponse', $response);
         //
+        // // Does it have all the original data we sent?
+        // $responseBody = $this->bodyFromResponse($response, 'updateSTW');
+        // // print_r($responseBody);
+        // $this->assertEquals($responseBody, $anotherSTWBody);
+
+
+        // Delete test entities:
+        // $delResponse = $this->schedules->deleteScheduleTimeWindowById(Configuration::$authorization, $responseSchedule->schedule->id, $response->schedule->timeWindows[1]->id);
+        $delScheduleResponse = $this->schedules->deleteScheduleById(Configuration::$authorization, $responseSchedule->schedule->id);
+        $delResponseResource = $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
+        $delResponseService = $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
     }
+
+    /**
+     * Test DELETE /schedules/{id}/time_windows/{time_window_id}
+     * GonebusyLib\Controllers\SchedulesController::deleteScheduleTimeWindowById()
+     */
+    public function testDeleteScheduleTimeWindowById() {
+        $serviceResponse = $this->services->createService(Configuration::$authorization, $this->createBody('Service'));
+        $resourceResponse = $this->resources->createResource(Configuration::$authorization, $this->createBody('Resource'));
+        // Create Schedule:
+        $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
+        $responseSchedule = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
+
+        // Create ScheduleTimeWindow:
+        $createSTWBody = $this->timeWindowBody('create');
+        $responseSTW = $this->schedules->createScheduleTimeWindow(Configuration::$authorization, $responseSchedule->schedule->id, $createSTWBody);
+
+
+        // Delete ScheduleTimeWindow:
+        $response = $this->schedules->deleteScheduleTimeWindowById(
+            Configuration::$authorization,
+            $responseSchedule->schedule->id,
+            $responseSTW->schedule->timeWindows[1]->id);
+
+        // Was it deleted?
+        $this->assertInstanceOf('GonebusyLib\Models\DeleteScheduleTimeWindowByIdResponse', $response);
+
+        $response->schedule->timeWindows[1] = $response->schedule->timeWindows[0];
+        // ^ XXX Hack for bodyFromResponse($response, 'createSTW')
+        $responseBody = $this->bodyFromResponse($response, 'createSTW');
+        $this->assertEquals($responseBody, $createSTWBody);
+        // The record previously created is the same as the one deleted.
+
+
+        // Delete remaining test entities:
+        $delScheduleResponse = $this->schedules->deleteScheduleById(Configuration::$authorization, $responseSchedule->schedule->id);
+        $delResponseResource = $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
+        $delResponseService = $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
+    }
+
 }
