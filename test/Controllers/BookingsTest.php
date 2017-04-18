4f1c25b3f0b38f7aa492963c7ff20804f9318a5d
Jorge Orpinel <jorge@orpinel.com>
2017-04-18 18:25:31 -0500

    createBooking test complete except for booking deletion...
    Getting {"message":"422 Booking is in state new and can not be canceled."} from API

--- test/Controllers/BookingsTest.php
+++ test/Controllers/BookingsTest.php
@@ -16,28 +16,30 @@ use GonebusyLib\Models\UpdateBookingByIdBody;
 
 class BookingsTest extends TestCase
 {
-    /**
-     * To contain the GonebusyLib\GonebusyClient
+    /** To contain the GonebusyLib\GonebusyClient
      */
     protected $client;
 
-    /**
-     * To contain the GonebusyLib\Controllers\BookingsController
+    /** To contain the GonebusyLib\Controllers\BookingsController
      */
     protected $bookings;
 
-    // To contain the GonebusyLib\Controllers\ServicesController
+    /** To contain a GonebusyLib\Controllers\ServicesController
+     */
     protected $services;
 
-    // To contain the GonebusyLib\Controllers\ResourcesController
+    /** To contain a GonebusyLib\Controllers\ResourcesController
+     */
     private $resources;
 
-    // To contain the GonebusyLib\Controllers\SchedulesController
+    /** To contain a GonebusyLib\Controllers\SchedulesController
+     */
     private $schedules;
 
 
     /**
-     * Create the GonebusyClient and BookingsController, etc for each test.
+     * Create the GonebusyClient and BookingsController,
+     * as well as other controllers (for every test).
      */
     public function setUp() {
         $this->client = new GonebusyClient();
@@ -50,15 +52,16 @@ class BookingsTest extends TestCase
 
     /**
      * Generate arbitrary service or resource data.
-     * @param  string $action 'Service' or 'Resource'
+     * @param  string $type 'Service' or 'Resource'
      * @return  mixed object for sending to API
      */
-    private function createBody($action) {
-        switch($action) {
+    private function createBody($type) {
+        switch($type) {
             case 'Service':
                 return new CreateServiceBody(
                     "description", // REQUIRED
-                    30, // duration REQUIRED
+                    15, // duration REQUIRED
+                    15, // max_duration optional but will default to duration
                     "name", // REQUIRED
                     NULL, // categories
                     NULL, // price_model_id
@@ -91,13 +94,13 @@ class BookingsTest extends TestCase
             NULL, // date_recurs_by
             "sunday, monday, tuesday, wednesday, thursday, friday, saturday", // days
             date('Y-m-d', strtotime('tomorrow')), // end_date
-            "18:00", // end_time
+            "17:00", // end_time
             NULL, // frequency defaults to 'every'
             NULL, // occurrence defaults to 'every'
             "daily", // recurs_by
             $resourceId,
             date('Y-m-d', strtotime('tomorrow')), // start_date
-            "12:00", // start_time
+            "09:00", // start_time
             NULL, // total_minutes can be deduced
             NULL // user_id defaults to self
         );
@@ -105,19 +108,19 @@ class BookingsTest extends TestCase
 
     /**
      * Generate unique user data.
-     * @param  string $type Should be 'create' or 'update'.
+     * @param  string $action Should be 'create' or 'update'.
      * @param  int $serviceId for new booking
      * @param  int $resourceId for new booking
      * @return  mixed object with unique data to send to API
      */
-    private function bookingBody($type, $serviceId=NULL, $resourceId=NULL) {
-        switch($type) {
+    private function bookingBody($action, $serviceId=NULL, $resourceId=NULL) {
+        switch($action) {
             case 'create':
                 return new CreateBookingBody(
                     date('Y-m-d', strtotime('tomorrow')), // date (within schedule start_date and end_date)
                     $serviceId, // service_id
                     "13:00", // time (within schedule start_time and end_date)
-                    30, // duration
+                    30, // duration (>= service durations)
                     $resourceId, // resource_id
                     NULL // user_id defaults to self
                 );
@@ -126,11 +129,29 @@ class BookingsTest extends TestCase
                     date('Y-m-d', strtotime('tomorrow')), // date (within schedule start_date and end_date)
                     $serviceId, // service_id
                     "13:45", // another time (within schedule start_time and end_date)
-                    15, // another duration
+                    15, // another duration (>= service durations)
                     $resourceId, // resource_id
                     NULL // user_id defaults to self
                 );
         }
+    }
+
+    /**
+     * Generate data from Booking response.
+     * @param  GonebusyLib\Models\EntitiesBookingsResponse $response
+     * @param  int $sId service ID (not in response)
+     * @param  int $rId resource ID (not in response)
+     * @return CreateBookingBody object with data from $response
+     */
+    private function bodyFromResponse($response, $sId, $rId) {
+        return new CreateBookingBody(
+            $response->booking->timeWindow->startDate, // date
+            $sId, // service_id
+            $response->booking->timeWindow->startTime, // time
+            $response->booking->timeWindow->totalMinutes, // duration
+            $rId, // resource_id
+            NULL // user_id defaults to self
+        );
     }
 
 
@@ -139,6 +160,7 @@ class BookingsTest extends TestCase
      * GonebusyLib\Controllers\UsersController::createBooking()
      */
     public function testCreateBooking() {
+        // echo("\n");
         // Create Service:
         $createServiceBody = $this->createBody('Service');
         $serviceResponse = $this->services->createService(Configuration::$authorization, $createServiceBody);
@@ -148,34 +170,35 @@ class BookingsTest extends TestCase
         // Give open Schedule for seervice/resource combination:
         $createScheduleBody = $this->scheduleBody($serviceResponse->service->id, $resourceResponse->resource->id);
         $scheduleResponse = $this->schedules->createSchedule(Configuration::$authorization, $createScheduleBody);
-        // print_r($scheduleResponse->schedule->timeWindows[0]);
+        // echo("Created a Schedule with this Time Window: "); print_r($scheduleResponse->schedule->timeWindows[0]);
 
 
         // Create Booking:
         $createBookingBody = $this->bookingBody('create', $serviceResponse->service->id, $resourceResponse->resource->id);
-        // print_r($createBookingBody);
         $response = $this->bookings->createBooking(
             Configuration::$authorization,
             $createBookingBody
         );
-        // print_r($response);
+        // echo("Created a Booking: "); print_r($response);
 
         // Was it created?
         $this->assertInstanceOf('GonebusyLib\Models\CreateBookingResponse', $response);
 
-        // TODO Does it have all the original data we sent?
-        // $responseBody = $this->bodyFromResponse($response, 'create');
-        // $this->assertEquals($responseBody, $createBookingBody);
+        // Does it have all the original data we sent?
+        $responseBody = $this->bodyFromResponse($response, $serviceResponse->service->id, $resourceResponse->resource->id);
+        $this->assertEquals($responseBody, $createBookingBody);
 
-        // TODO Delete test booking:
+        // XXX Delete test booking:
+        // $this->bookings->cancelBookingById(Configuration::$authorization, $response->booking->id);
 
 
-        // TODO Delete test schedule:
-        // TODO test resource:
+        // Delete test schedule:
+        $this->schedules->deleteScheduleById(Configuration::$authorization, $scheduleResponse->schedule->id);
+        // Delete test resource:
+        $this->resources->deleteResourceById(Configuration::$authorization, $resourceResponse->resource->id);
         // Delete test service:
-        $delResponse = $this->services->deleteServiceById(Configuration::$authorization, $responseService->service->id);
+        $this->services->deleteServiceById(Configuration::$authorization, $serviceResponse->service->id);
     }
-
 
     /**
      * Test PUT /bookings/{id}
